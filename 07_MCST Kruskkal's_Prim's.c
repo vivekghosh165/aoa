edaa6f359d743240b4fb6063e48fd8b27de908f3
Vivek Ghosh <vivekghosh165@gmail.com>
2023-04-25 11:39:44 +0530

    Update 07_MCST Kruskkal's_Prim's.c

--- 07_MCST Kruskkal's_Prim's.c
+++ 07_MCST Kruskkal's_Prim's.c
@@ -1,13 +1,10 @@
+//07_MCST Kruskkal's_Prim's.c
 #include<stdio.h>
-#include<stdlib.h>
- 
+#include<stdlib.h> 
 #define infinity 9999
 #define MAX 20
- 
 int G[MAX][MAX],spanning[MAX][MAX],n;
- 
 int prims();
- 
 int main()
 {
 int i,j,total_cost;
@@ -28,13 +25,11 @@ printf("%d\t",spanning[i][j]);
 printf("\n\nTotal cost of spanning tree=%d",total_cost);
 return 0;
 }
- 
 int prims()
 {
 int cost[MAX][MAX];
 int u,v,min_distance,distance[MAX],from[MAX];
 int visited[MAX],no_of_edges,i,min_cost,j;
-//create cost[][] matrix,spanning[][]
 for(i=0;i<n;i++)
 for(j=0;j<n;j++)
 {
@@ -44,7 +39,6 @@ else
 cost[i][j]=G[i][j];
 spanning[i][j]=0;
 }
-//initialise visited[],distance[] and from[]
 distance[0]=0;
 visited[0]=1;
 for(i=1;i<n;i++)
@@ -53,11 +47,10 @@ distance[i]=cost[0][i];
 from[i]=0;
 visited[i]=0;
 }
-min_cost=0; //cost of spanning tree
-no_of_edges=n-1; //no. of edges to be added
+min_cost=0;
+no_of_edges=n-1;
 while(no_of_edges>0)
 {
-//find the vertex at minimum distance from the tree
 min_distance=infinity;
 for(i=1;i<n;i++)
 if(visited[i]==0&&distance[i]<min_distance)
@@ -66,12 +59,10 @@ v=i;
 min_distance=distance[i];
 }
 u=from[v];
-//insert the edge in spanning tree
 spanning[u][v]=distance[v];
 spanning[v][u]=distance[v];
 no_of_edges--;
 visited[v]=1;
-//updated the distance[] array
 for(i=1;i<n;i++)
 if(visited[i]==0&&cost[i][v]<distance[i])
 {
