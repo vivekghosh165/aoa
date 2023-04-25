94ee76a6383fbbcf3b74450ea91c9757f099a67a
Vivek Ghosh <vivekghosh165@gmail.com>
2023-04-25 12:01:58 +0530

    Update 11_Graph_Colouring.c

--- 11_Graph_Colouring.c
+++ 11_Graph_Colouring.c
@@ -1,12 +1,10 @@
+//11_Graph_Colouring.c
 #include <stdio.h>
 #include <stdlib.h>
-
 #define MAX 100
-
 int graph[MAX][MAX];
 int color[MAX];
 int n;
-
 int is_safe(int vertex, int c)
 {
     int i;
@@ -17,7 +15,6 @@ int is_safe(int vertex, int c)
     }
     return 1;
 }
-
 int graph_coloring(int vertex)
 {
     int c;
@@ -35,25 +32,20 @@ int graph_coloring(int vertex)
     }
     return 0;
 }
-
 int main()
 {
     int i, j;
-
     printf("Enter the number of vertices (max %d): ", MAX);
     scanf("%d", &n);
-
     printf("Enter the adjacency matrix:\n");
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
             scanf("%d", &graph[i][j]);
         }
     }
-
     for (i = 0; i < n; i++) {
         color[i] = 0;
     }
-
     if (graph_coloring(0)) {
         printf("The graph can be colored with %d colors:\n", n);
         for (i = 0; i < n; i++) {
@@ -62,6 +54,5 @@ int main()
     } else {
         printf("The graph cannot be colored with %d colors.\n", n);
     }
-
     return 0;
 }
