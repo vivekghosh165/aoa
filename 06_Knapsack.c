3de0a3f3cdad270184997411ba705aab09d7b837
Vivek Ghosh <vivekghosh165@gmail.com>
2023-04-25 19:39:54 +0530

    Update 06_Knapsack.c

--- 06_Knapsack.c
+++ 06_Knapsack.c
@@ -1,9 +1,8 @@
+//06_Knapsack.c
 #include<stdio.h>
 #define MAX_N 20
 #define MAX_W 100
-
 int max(int a, int b) { return (a > b)? a : b; }
-
 int knapSack(int W, int wt[], int val[], int n)
 {
    int i, w;
@@ -22,22 +21,17 @@ int knapSack(int W, int wt[], int val[], int n)
    }
    return K[n][W];
 }
-
 int main()
 {
     int i, n, val[MAX_N], wt[MAX_N], W;
-    
     printf("Enter number of items: ");
     scanf("%d", &n);
-    
     printf("Enter value and weight of items:\n");
     for(i = 0;i < n; ++i){
      scanf("%d%d", &val[i], &wt[i]);
     }
- 
     printf("Enter size of knapsack: ");
     scanf("%d", &W);
-    
     printf("%d\n", knapSack(W, wt, val, n));
     return 0;
 }
